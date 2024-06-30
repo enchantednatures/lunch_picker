a131925d236cb0990d36d2c0b2a857b06ba7963f
hcasten <huntercasten@icloud.com>
2024-06-29 22:39:02 -0600

    feat(sqlite): intial work on feature flagging

---
+++ tests/fixtures/postgres/homies.sql
@@ -0,0 +1,16 @@
+insert into public.homies (id, user_id, name)
+values  (1, 1, 'Mirabel'),
+        (2, 1, 'Dale'),
+        (3, 1, 'Eleanora'),
+        (4, 1, 'Rasla'),
+        (5, 1, 'Kari'),
+        (6, 1, 'Lavina'),
+        (7, 1, 'Reeba'),
+        (8, 1, 'Gilbertine'),
+        (9, 1, 'Lizzie'),
+        (10, 1, 'Danika'),
+        (11, 1, 'Hunter'),
+        (12, 1, 'Aaren'),
+        (13, 1, 'Aarika'),
+        (14, 1, 'Abagael'),
+        (15, 1, 'Abagail');
---
+++ tests/fixtures/postgres/homies_favorite_restaurants.sql
@@ -0,0 +1,15 @@
+insert into public.homies_favorite_restaurants (homie_id, restaurant_id, user_id)
+values  (1, 3, 1),
+        (1, 10, 1),
+        (1, 9, 1),
+        (1, 22, 1),
+        (1, 4, 1),
+        (11, 3, 1),
+        (1, 17, 1),
+        (1, 18, 1),
+        (1, 19, 1),
+        (1, 1, 1),
+        (1, 13, 1),
+        (11, 2, 1),
+        (11, 1, 1),
+        (11, 56, 1);
